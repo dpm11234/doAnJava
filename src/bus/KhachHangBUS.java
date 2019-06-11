0e2b5eb6a98bbffab1b204f94d821fd584950c3d
dpm11234 <dpm11234@gmail.com>
2019-06-11 21:41:34 +0700

    fix validate

--- src/bus/KhachHangBUS.java
+++ src/bus/KhachHangBUS.java
@@ -14,13 +14,13 @@ public class KhachHangBUS {
     public static int addCustomer(KhachHangDTO khachHang) {
         String regexName = "[a-z]{2,}";
 
-        if(khachHang.getHoTen().equals("Họ tên") && !khachHang.getHoTen().matches(regexName)) {
+        if(khachHang.getHoTen().equals("Họ tên") || !khachHang.getHoTen().matches(regexName)) {
             return -2;
         }
 
         String regexPhone = "^0[0-9]{9}";
 
-        if(khachHang.getSdt().equals("Số điện thoại") && !khachHang.getSdt().matches(regexPhone)) {
+        if(khachHang.getSdt().equals("Số điện thoại") || !khachHang.getSdt().matches(regexPhone)) {
             return -3;
         }
 
